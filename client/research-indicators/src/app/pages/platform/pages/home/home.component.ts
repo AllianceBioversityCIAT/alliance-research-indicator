0de371812c2a8340f118dbc0ec27d2bd5fd2530b
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-08-02 15:22:17 -0500

    :recycle: refactor: root colors added

--- client/research-indicators/src/app/pages/platform/pages/home/home.component.ts
+++ client/research-indicators/src/app/pages/platform/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HomeHeaderComponent } from '../../../../shared/components/home-header/home-header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HomeHeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
