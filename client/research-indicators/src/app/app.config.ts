c67f526add9a0ad6f00305be701f59a1f544a837
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-09-11 11:54:15 -0500

    :recycle: refactor: webSocketServerUrl added

--- client/research-indicators/src/app/app.config.ts
+++ client/research-indicators/src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-const config: SocketIoConfig = { url: 'http://localhost:3005', options: {} };
+import { environment } from '../environments/environment';
+const config: SocketIoConfig = { url: environment.webSocketServerUrl, options: {} };
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withViewTransitions()), provideHttpClient(), importProvidersFrom(BrowserAnimationsModule, SocketIoModule.forRoot(config))]
