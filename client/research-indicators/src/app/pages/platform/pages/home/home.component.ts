4e9a28059930c1a8ebfec6ed37df5fd1cd3d1e75
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-09-19 09:03:40 -0500

    :recycle: refactor: home-header component removed

--- client/research-indicators/src/app/pages/platform/pages/home/home.component.ts
+++ client/research-indicators/src/app/pages/platform/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { HomeHeaderComponent } from '../../../../shared/components/home-header/home-header.component';
 import { ResultsTableComponent } from '../../../../shared/components/results-table/results-table.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeHeaderComponent, ResultsTableComponent],
+  imports: [ResultsTableComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
