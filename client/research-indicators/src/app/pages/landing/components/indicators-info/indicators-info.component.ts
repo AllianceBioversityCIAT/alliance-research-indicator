c7b714ecbb40b54eb960fe7fa4eabd134848306a
Julian Sanchez <julian.sanchez@cgiar.org>
2024-09-24 15:12:44 -0500

    :sparkles: feat: indicators-info and services components created

---
+++ client/research-indicators/src/app/pages/landing/components/indicators-info/indicators-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-indicators-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './indicators-info.component.html',
+  styleUrl: './indicators-info.component.scss'
+})
+export class IndicatorsInfoComponent {
+
+}
