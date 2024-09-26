60edff62584b749d05db339fa9d957431e3cb83a
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-09-26 15:42:42 -0500

    :sparkles: feat: navbar updated and notification button added
    Co-Authored-By: Kevin Collazos <61396222+xKeCo@users.noreply.github.com>

--- client/research-indicators/src/app/shared/components/alliance-navbar/alliance-navbar.component.ts
+++ client/research-indicators/src/app/shared/components/alliance-navbar/alliance-navbar.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { BadgeModule } from 'primeng/badge';
 import { ChipModule } from 'primeng/chip';
@@ -6,27 +6,47 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { DynamicToastService } from '../../services/dynamic-toast.service';
 import { CacheService } from '../../services/cache.service';
 import { DarkModeService } from '../../services/dark-mode.service';
+import { MenuModule } from 'primeng/menu';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'alliance-navbar',
   standalone: true,
-  imports: [ButtonModule, BadgeModule, ChipModule, RouterLink, RouterLinkActive],
+  imports: [ButtonModule, BadgeModule, ChipModule, RouterLink, RouterLinkActive, MenuModule, AvatarModule],
   templateUrl: './alliance-navbar.component.html',
   styleUrl: './alliance-navbar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AllianceNavbarComponent {
+export class AllianceNavbarComponent implements OnInit {
   dynamicToast = inject(DynamicToastService);
   cache = inject(CacheService);
   darkModeService = inject(DarkModeService);
   router = inject(Router);
+  items: any;
+
   options = [
     { label: 'Home', path: '/home' },
-    { label: 'About Indicators', path: '/about' },
-    { label: 'Notifications', path: '/notifications' },
-    { label: 'Settings', path: '/settings' },
-    { label: 'Profile', path: '/profile' }
+    { label: 'My Results', path: '/about', options: [{}] },
+    { label: 'My Contracts', path: '/notifications' }
   ];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Options',
+        items: [
+          {
+            label: 'Refresh',
+            icon: 'pi pi-refresh'
+          },
+          {
+            label: 'Export',
+            icon: 'pi pi-upload'
+          }
+        ]
+      }
+    ];
+  }
 
   logOut() {
     localStorage.removeItem('decoded');
