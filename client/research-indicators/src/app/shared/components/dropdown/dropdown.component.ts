04b730eb3d5461d1722e3f7e63f5369ca31f7614
Julian Sanchez <julian.sanchez@cgiar.org>
2024-10-18 09:13:20 -0500

    :sparkles: feat: dropdown component developed

--- client/research-indicators/src/app/shared/components/dropdown/dropdown.component.ts
+++ client/research-indicators/src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './dropdown.component.scss'
 })
 export class DropdownComponent {
+  @Input() position: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right' = 'bottom-right';
 
+  isOpen = false;
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  closeDropdown() {
+    this.isOpen = false;
+  }
 }
